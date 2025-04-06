640845669d5aa9fcdb58bebd832e40a4ba73da35
Anan <abdullaanan017@gmail.com>
2025-04-06 20:24:23 +0530

    final

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,73 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { MantineProvider } from "@mantine/core";
+
+// import "./App.css";
+// import Dashboard from "./components/Dashboard/Dashboard";
+// import Expense from "./components/Expense/Expense";
+// import Home from "./components/Home/Home";
+// import Signup from "./components/Signup/Signup";
+// import Login from "./components/Login/Login";
+// import ProtectedRoute from "./components/ProtectedRoutes/ProtectedRoute";
+// import News from "./components/Setting/News";
+// import Portfolio from "./components/Portfolio/Portfolio";
+// import Budget from "./components/Budget/Budget";
+// import Help from './components/help/help'
+
+// function App() {
+//   return (
+//     <MantineProvider theme={{}}>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Home />}></Route>
+//           <Route path="/login" element={<Login />}></Route>
+//           <Route path="/signup" element={<Signup />}></Route>
+//           <Route
+//             path="/dashboard/:username"
+//             element={
+//               <ProtectedRoute>
+//                 <Dashboard />
+//               </ProtectedRoute>
+//             }
+//           />
+//           <Route
+//             path="/portfolio/:username"
+//             element={
+//               <ProtectedRoute>
+//                 <Portfolio />
+//               </ProtectedRoute>
+//             }
+//           />
+//           <Route
+//             path="/expense/:username"
+//             element={
+//               <ProtectedRoute>
+//                 <Expense />
+//               </ProtectedRoute>
+//             }
+//           />
+//           <Route
+//             path="/budget/:username"
+//             element={
+//               <ProtectedRoute>
+//                 <Budget />
+//               </ProtectedRoute>
+//             }
+//           />
+//           <Route
+//             path="/News/:username"
+//             element={
+//               <ProtectedRoute>
+//                 <News />
+//               </ProtectedRoute>
+//             }
+//           />
+//         </Routes>
+//       </BrowserRouter>
+//     </MantineProvider>
+//   );
+// }
+
+// export default App;
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MantineProvider } from "@mantine/core";
 
@@ -11,15 +81,16 @@ import ProtectedRoute from "./components/ProtectedRoutes/ProtectedRoute";
 import News from "./components/Setting/News";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Budget from "./components/Budget/Budget";
+import Help from "./components/help/help"; 
 
 function App() {
   return (
     <MantineProvider theme={{}}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route
             path="/dashboard/:username"
             element={
@@ -53,10 +124,18 @@ function App() {
             }
           />
           <Route
-            path="/News/:username"
+            path="/news/:username"
             element={
               <ProtectedRoute>
                 <News />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/help/:username"
+            element={
+              <ProtectedRoute>
+                <Help />
               </ProtectedRoute>
             }
           />
--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -359,13 +359,220 @@
 //   );
 // };
 // export default Sidebar;
+// import { useNavigate } from "react-router";
+// import { Link } from "react-router-dom";
+// import { useSelector, useDispatch } from "react-redux";
+// import { createStyles, Navbar, Group, Button } from "@mantine/core";
+// import { useMediaQuery } from "@mantine/hooks";
+// import { Logout } from "tabler-icons-react";
+// import { News } from "tabler-icons-react";
+// import { RootState } from "../../Redux/store";
+// import { setIsLoggedIn } from "../../Redux/userSlice";
+// import { setActiveItem } from "../../Redux/helperSlice";
+// import { UserInfo } from "../UserInfo/UserInfo";
+// import { NavigationLinkData } from "../../data/NavigationLinkData";
+
+// const useStyles = createStyles((theme) => ({
+//   navbar: {
+//     backgroundColor: theme.colors.indigo[6],
+//     borderColor: theme.colors.indigo[6],
+//     position: "fixed",
+//     minHeight: "100%",
+//     top: 0,
+//   },
+//   brand: {
+//     fontSize: 28,
+//     fontWeight: 700,
+//     color: theme.white,
+//     marginBottom: theme.spacing.md,
+//     textAlign: "center",
+//     fontFamily: "Montserrat, sans-serif",
+//   },
+//   header: {
+//     paddingBottom: theme.spacing.md,
+//     marginBottom: theme.spacing.md * 1.5,
+//   },
+//   footer: {
+//     paddingTop: theme.spacing.sm,
+//     marginTop: theme.spacing.sm,
+//     paddingBottom: theme.spacing.sm,
+//     borderTop: `1px solid ${theme.colors.blue[9]}`,
+//   },
+//   link: {
+//     display: "flex",
+//     alignItems: "center",
+//     textDecoration: "none",
+//     fontSize: theme.fontSizes.md,
+//     color: theme.white,
+//     padding: `${theme.spacing.md}px ${theme.spacing.xs}px`,
+//     borderRadius: theme.radius.sm,
+//     fontWeight: 500,
+//     "&:hover": {
+//       backgroundColor: theme.colors[theme.primaryColor][5],
+//     },
+//   },
+//   linkIcon: {
+//     color: theme.white,
+//     opacity: 0.75,
+//     marginRight: theme.spacing.sm,
+//   },
+//   linkActive: {
+//     "&, &:hover": {
+//       backgroundColor: theme.colors.blue[9],
+//     },
+//   },
+//   logoutLink: {
+//     display: "flex",
+//     alignItems: "center",
+//     textDecoration: "none",
+//     fontSize: theme.fontSizes.sm,
+//     color: theme.white,
+//     padding: `${theme.spacing.sm}px ${theme.spacing.sm}px`,
+//     borderRadius: theme.radius.sm,
+//     fontWeight: 500,
+//     "&:hover": {
+//       backgroundColor: theme.colors[theme.primaryColor][5],
+//     },
+//   },
+// }));
+
+// const Sidebar = () => {
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+//   const { navLinkData } = NavigationLinkData();
+
+//   const userData = useSelector((state: RootState) => state.user.user);
+//   const { email, username, image } = userData;
+//   const imageUrlArray = image.map((image: any) => image.imageUrl);
+//   const userImage = imageUrlArray[imageUrlArray.length - 1];
+
+//   const { classes, cx } = useStyles();
+//   const largeScreen = useMediaQuery("(min-width: 1300px)");
+//   const active = useSelector((state: RootState) => state.helper.activeItem);
+
+//   const handleLogout = () => {
+//     dispatch(setIsLoggedIn());
+//     dispatch(setActiveItem("Dashboard"));
+//     localStorage.removeItem("currentToken");
+//     localStorage.removeItem("username");
+//     localStorage.removeItem("userId");
+//   };
+
+//   const links = navLinkData.map((item) => {
+//     if (item.label === "Dashboard") {
+//       return (
+//         <>
+//           <Link
+//             className={cx(classes.link, {
+//               [classes.linkActive]: item.label === active,
+//             })}
+//             to={item.link}
+//             key={item.label}
+//             onClick={(event) => {
+//               event.preventDefault();
+//               navigate(item.link);
+//               dispatch(setActiveItem(item.label));
+//             }}
+//           >
+//             <item.icon className={classes.linkIcon} />
+//             <span>{item.label}</span>
+//           </Link>
+
+//           {/* Portfolio added below Dashboard */}
+//           <Link
+//             className={cx(classes.link, {
+//               [classes.linkActive]: "Portfolio" === active,
+//             })}
+//             to={`/portfolio/${username}`}
+//             key={"Portfolio"}
+//             onClick={(event) => {
+//               event.preventDefault();
+//               navigate(`/portfolio/${username}`);
+//               dispatch(setActiveItem("Portfolio"));
+//             }}
+//           >
+//             <item.icon className={classes.linkIcon} />
+//             <span>Portfolio</span>
+//           </Link>
+//         </>
+//       );
+//     } else if (item.label === "Expense" || item.label === "Budget") {
+//       return (
+//         <Link
+//           className={cx(classes.link, {
+//             [classes.linkActive]: item.label === active,
+//           })}
+//           to={item.link}
+//           key={item.label}
+//           onClick={(event) => {
+//             event.preventDefault();
+//             navigate(item.link);
+//             dispatch(setActiveItem(item.label));
+//           }}
+//         >
+//           <item.icon className={classes.linkIcon} />
+//           <span>{item.label}</span>
+//         </Link>
+//       );
+//     }
+//     return null;
+//   });
+
+//   // Add News link manually
+//   links.push(
+//     <Link
+//       className={cx(classes.link, {
+//         [classes.linkActive]: "News" === active,
+//       })}
+//       to={`/news/${username}`}
+//       key={"News"}
+//       onClick={(event) => {
+//         event.preventDefault();
+//         navigate(`/news/${username}`);
+//         dispatch(setActiveItem("News"));
+//       }}
+//     >
+//       <News className={classes.linkIcon} />
+//       <span>News</span>
+//     </Link>
+//   );
+  
+
+//   return (
+//     <Navbar
+//       width={{ sm: largeScreen ? 300 : 200 }}
+//       p="md"
+//       className={classes.navbar}
+//     >
+//       <Navbar.Section mt={-45}>
+//         <Group className={classes.header} position="apart" />
+//         <div className={classes.brand}>FinTrack</div>
+//         <UserInfo avatar={userImage} name={username} email={email} />
+//         {links}
+//       </Navbar.Section>
+
+//       <Navbar.Section className={classes.footer}>
+//         <Button
+//           variant="subtle"
+//           size="md"
+//           className={classes.logoutLink}
+//           onClick={handleLogout}
+//         >
+//           <Logout className={classes.linkIcon} />
+//           <span>Logout</span>
+//         </Button>
+//       </Navbar.Section>
+//     </Navbar>
+//   );
+// };
+
+// export default Sidebar;
 import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { createStyles, Navbar, Group, Button } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
-import { Logout } from "tabler-icons-react";
-import { News } from "tabler-icons-react";
+import { Logout, News, Help } from "tabler-icons-react"; // ✅ Correct icons
 import { RootState } from "../../Redux/store";
 import { setIsLoggedIn } from "../../Redux/userSlice";
 import { setActiveItem } from "../../Redux/helperSlice";
@@ -534,6 +741,25 @@ const Sidebar = () => {
     >
       <News className={classes.linkIcon} />
       <span>News</span>
+    </Link>
+  );
+
+  // Add Help link manually
+  links.push(
+    <Link
+      className={cx(classes.link, {
+        [classes.linkActive]: "Help" === active,
+      })}
+      to={`/help/${username}`}
+      key={"Help"}
+      onClick={(event) => {
+        event.preventDefault();
+        navigate(`/help/${username}`);
+        dispatch(setActiveItem("Help"));
+      }}
+    >
+      <Help className={classes.linkIcon} />
+      <span>Help</span>
     </Link>
   );
 
